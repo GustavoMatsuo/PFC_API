66a4a4f6264334b72ec69a7f59801c077aa0068b
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2022-05-21 17:57:57 -0300

    fix: removing props public on user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,26 +5,26 @@ import { v4 as uuidv4 } from 'uuid'
 @Entity('user')
 export class User {
   @PrimaryColumn()
-  public id:string
+  id:string
 
   @Column()
-  public status:boolean
+  status:boolean
 
   @Column()
-  public name:string
+  name:string
 
   @Column({
     type: "enum",
     enum: UserRole,
     default: UserRole.EMP
   })
-  public role:string
+  role:string
 
   @Column()
-  public email:string
+  email:string
 
   @Column()
-  public password:string
+  password:string
 
   constructor(props: Omit<User, 'id'>, id?:string) {
     Object.assign(this, props)
