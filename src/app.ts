1870c1e20164e01aaedb4df4003cf089bceb1702
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2022-05-21 17:20:46 -0300

    chore: add .gitignore and cors in the project

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { router } from './routes'
-import dotenv from "dotenv"
+import dotenv from 'dotenv'
+import cors from 'cors'
 
 const app = express()
 dotenv.config()
 
+app.use(cors('*'))
 app.use(express.json())
 app.use(router)
 
