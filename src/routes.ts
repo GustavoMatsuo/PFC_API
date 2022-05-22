d2c415e6ae286430f8d55fcb3edeb49c34d8ef39
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2022-05-22 15:15:34 -0300

    chore: add routes to Produto and Categoria

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express"
+import { categoriaController } from "./components/Categoria"
 import { fornecedorController } from "./components/Fornecedor"
+import { produtoController } from "./components/Produto"
 import { userController } from "./components/User"
 
 const router = Router()
@@ -36,6 +38,40 @@ router.put('/fornecedor', (request, response) => {
 })
 router.put('/fornecedor/status', (request, response) => {
   return fornecedorController.changeStatus(request, response)
+})
+
+//CATEGORIA
+router.post('/categoria', (request, response) => {
+  return categoriaController.create(request, response)
+})
+router.get('/categoria', (request, response) => {
+  return categoriaController.read(request, response)
+})
+router.put('/categoria', (request, response) => {
+  return categoriaController.update(request, response)
+})
+router.delete('/categoria', (request, response) => {
+  return categoriaController.delete(request, response)
+})
+router.get('/categoria/list', (request, response) => {
+  return categoriaController.index(request, response)
+})
+
+//PRODUTO
+router.get('/produto', (request, response) => {
+  return produtoController.index(request, response)
+})
+router.post('/produto', (request, response) => {
+  return produtoController.create(request, response)
+})
+router.put('/produto', (request, response) => {
+  return produtoController.update(request, response)
+})
+router.put('/produto/status', (request, response) => {
+  return produtoController.changeStatus(request, response)
+})
+router.put('/produto/updateEstoque', (request, response) => {
+  return produtoController.updateEstoque(request, response)
 })
 
 export { router }
